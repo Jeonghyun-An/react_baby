6ea10d0f0e52760cb9f1ffe720c6b74a1b7de011
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-06-09 14:57:23 +0900

    tic tac toe game init!

---
+++ react_baby/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+        setValue("X");
+    }
+    return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    );
+}
+
+export default function Board() {
+    return (
+        <>
+            <div classname="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div classname="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div classname="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    );
+}
