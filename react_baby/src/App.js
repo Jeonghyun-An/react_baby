24818e93187049af925ca8cf311bfcd9208d82f7
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-06-09 15:19:32 +0900

    state로 변경

--- react_baby/src/App.js
+++ react_baby/src/App.js
@@ -1,25 +1,35 @@
 import { useState } from "react";
 
-function Square() {
-    const [value, setValue] = useState(null);
-
-    function handleClick() {
-        setValue("X");
-    }
+function Square({ value, onSquareClick }) {
     return (
-        <button className="square" onClick={handleClick}>
+        <button className="square" onClick={onSquareClick}>
             {value}
         </button>
     );
 }
 
 export default function Board() {
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    function handleClick(i) {
+        const nextSquares = squares.slice();
+        nextSquares[i] = "X";
+        setSquares(nextSquares);
+    }
     return (
         <>
             <div classname="board-row">
-                <Square />
-                <Square />
-                <Square />
+                <Square
+                    value={squares[0]}
+                    onSquareClick={() => handleClick(0)}
+                />
+                <Square
+                    value={squares[1]}
+                    onSquareClick={() => handleClick(1)}
+                />
+                <Square
+                    value={squares[2]}
+                    onSquareClick={() => handleClick(2)}
+                />{" "}
             </div>
             <div classname="board-row">
                 <Square />
